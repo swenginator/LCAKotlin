82bc015fb180ed4719ce53db7daf9112f5a0edf1
swenginator <72451636+swenginator@users.noreply.github.com>
2020-10-20 18:03:57 +0100

    Remove main method

--- src/main/kotlin/LCA.kt
+++ src/main/kotlin/LCA.kt
@@ -1,23 +1,9 @@
-import java.util.*
+package main.kotlin
+
+import kotlin.collections.HashSet
 
 /** Define a node class that holds a value and a set of children **/
 class Node<V>(val data: V, val children: MutableSet<Node<V>> = HashSet())
-
-fun main() {
-    // Create a test tree
-    val root = Node(0)
-    val node1 = Node(1)
-    val node2 = Node(2)
-    val node3 = Node(3)
-    val node4 = Node(4)
-    root.children.add(node1)
-    root.children.add(node2)
-    node1.children.add(node3)
-    node1.children.add(node4)
-
-    val lca = getLCA(root, node3, node4)
-    println("LCA: " + lca?.data)
-}
 
 fun <V> getLCA(root: Node<V>, nodea: Node<V>, nodeb: Node<V>): Node<V>? {
     if (nodea === root || nodeb === root) return root
